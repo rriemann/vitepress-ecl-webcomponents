13e544ac1109127f1e1bd705194d9ae1ffaaf48d
Robert Riemann <robert.riemann@edps.europa.eu>
2026-04-17 00:20:24 +0200

    initial commit: npx vitepress init

---
+++ .vitepress/config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "VitePress with ECL Webcomponents",
+  description: "Feasibility Study"
+})
