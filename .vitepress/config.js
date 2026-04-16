1b67ee6194d769a82fdf1a9b73ab9d524378aa47
Robert Riemann <robert.riemann@edps.europa.eu>
2026-04-17 00:35:51 +0200

    add ecl-webcomponents-vue

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vitepress'
 
+
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "VitePress with ECL Webcomponents",
-  description: "Feasibility Study"
+  description: "Feasibility Study",
+  vite: {
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            // treat all tags with a dash as custom elements
+            isCustomElement: (tag) => tag.includes('-'),
+          },
+        },
+      }),
+      vueJsx(),
+    ]
+  },
+  head: [
+    [
+      'meta', {'data-ecl-asset-path': '/'}
+    ]
+  ],
 })
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,11 +2,14 @@
 import Layout from './Layout.vue'
 import './style.css'
 
+import { ComponentLibrary } from '@ecl/ecl-webcomponents-vue'
+
 /** @type {import('vitepress').Theme} */
 export default {
   Layout,
   enhanceApp({ app, router, siteData }) {
     // ...
+    app.use(ComponentLibrary)
   }
 }
 
